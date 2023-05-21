5f1e2d2e981f6726ee9b1ac1651393e7dbe0f441
alopacin <adlopacinski@gmail.com>
2023-05-21 21:31:11 +0200

    poprawiona mechanika programu

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 liczba_produktow = int(input('Ile produktow chcesz wysłać?: '))
-liczba_paczek = 1
-liczba_kilogramow = 0
-liczba_pustych_kilogramow = 0
+ilosc_wyslanych_paczek = 1
+laczna_liczba_kilogramow = 0
+suma_pustych_kilogramow = 0
 waga_przedmiotu = 0
 idx = 0
-if liczba_produktow =
+jedna_paczka = 0
+
 for idx in range(liczba_produktow):
     waga_przedmiotu += int(input('Podaj wagę przedmiotu: '))
-    waga_przedmiotu += liczba_kilogramow
-    waga_przedmiotu -= waga_przedmiotu
-    idx += 1
-    if waga_przedmiotu >= 20:
-        liczba_kilogramow -= liczba_kilogramow
-        liczba_paczek += 1
+    jedna_paczka += waga_przedmiotu
+    if waga_przedmiotu < 1 or waga_przedmiotu > 10:
+        break
+    elif jedna_paczka < 20:
+        laczna_liczba_kilogramow += waga_przedmiotu
+        waga_przedmiotu -= waga_przedmiotu
+    elif jedna_paczka > 20:
+        laczna_liczba_kilogramow += waga_przedmiotu
+        ilosc_wyslanych_paczek += 1
+        jedna_paczka -= jedna_paczka
+        suma_pustych_kilogramow += waga_przedmiotu
+    elif jedna_paczka == 20 :
+        laczna_liczba_kilogramow += waga_przedmiotu
+        ilosc_wyslanych_paczek += 1
+        jedna_paczka -= jedna_paczka
+        waga_przedmiotu -= waga_przedmiotu
 
 
-print(liczba_kilogramow)
-print(liczba_paczek)
+print(f'Wysłano : {laczna_liczba_kilogramow} kg')
+print(f'Wysłano : {ilosc_wyslanych_paczek} paczek')
+print(f'Suma pustych kilogramów: {suma_pustych_kilogramow}')
 
 
 
