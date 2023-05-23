742658ccfab97161c471647cb59b321bf28e7be9
alopacin <adlopacinski@gmail.com>
2023-05-23 23:57:51 +0200

    walka trwa

--- main.py
+++ main.py
@@ -5,32 +5,39 @@ ilosc_wyslanych_paczek = 1
 laczna_liczba_kilogramow = 0
 suma_pustych_kilogramow = 0
 produkt = 0
-idx = 0
+idx = []
 nadmiar = 0
 paczka = 0
-liczba_produktow = int(input('Ile produktów chcesz wysłać?: '))
-
 # wlasciwa czesc programu
 
-for idx in range(liczba_produktow):
-    idx += 1
-    produkt -= produkt
-    produkt += int(input('Podaj wagę produktu: '))
+liczba_produktow = int(input('Ile produktów chcesz wysłać?: '))
+for _ in range(liczba_produktow):
+    produkt = int(input('Podaj wagę produktu: '))
     paczka += produkt
     if produkt < 1 or produkt > 10:
         break
     elif paczka <= LIMIT_JEDNEJ_PACZKI:
         laczna_liczba_kilogramow += produkt
+        produkt = 0
+        continue
+    elif paczka == LIMIT_JEDNEJ_PACZKI :
+        laczna_liczba_kilogramow += produkt
+        ilosc_wyslanych_paczek += 1
+        paczka = 0
+        produkt = 0
         continue
     elif paczka > LIMIT_JEDNEJ_PACZKI:
-        ilosc_wyslanych_paczek += 1
-        laczna_liczba_kilogramow += produkt
-        nadmiar = paczka - LIMIT_JEDNEJ_PACZKI
+        paczka -= produkt
+        nadmiar = LIMIT_JEDNEJ_PACZKI - paczka
         suma_pustych_kilogramow += nadmiar
-        nadmiar -= nadmiar
-        paczka -= paczka
-        print('Paczka się zapełniła')
-if ilosc_wyslanych_paczek <= 1:
+        laczna_liczba_kilogramow += paczka
+        paczka += produkt
+        nadmiar = 0
+        paczka = 0
+        produkt = 0
+        ilosc_wyslanych_paczek += 1
+        continue
+if ilosc_wyslanych_paczek <= 1 :
     suma_pustych_kilogramow = LIMIT_JEDNEJ_PACZKI - paczka
 
 # wyswietlenie wynikow
@@ -43,6 +50,7 @@ else:
     print(print(f'Wysłano : {ilosc_wyslanych_paczek} paczek'))
 print(f'Wysłano : {laczna_liczba_kilogramow} kg')
 print(f'Suma pustych kilogramów : {suma_pustych_kilogramow}')
+
 
 
 
