9e5a77a87a27cde6c9e4d033fe05c01d43a5bf56
alopacin <adlopacinski@gmail.com>
2023-05-27 23:30:17 +0200

    zapisanie pracy

--- main.py
+++ main.py
@@ -4,41 +4,49 @@ LIMIT_JEDNEJ_PACZKI = 20
 ilosc_wyslanych_paczek = 1
 laczna_liczba_kilogramow = 0
 suma_pustych_kilogramow = 0
-produkt = 0
-idx = []
 nadmiar = 0
 paczka = 0
-# wlasciwa czesc programu
 
+# wlasciwa czesc programu
 liczba_produktow = int(input('Ile produktów chcesz wysłać?: '))
 for _ in range(liczba_produktow):
     produkt = int(input('Podaj wagę produktu: '))
     paczka += produkt
+    print(paczka)
+    print(produkt)
+    print(laczna_liczba_kilogramow)
+    print(suma_pustych_kilogramow)
     if produkt < 1 or produkt > 10:
         break
-    elif paczka <= LIMIT_JEDNEJ_PACZKI:
+    elif paczka < LIMIT_JEDNEJ_PACZKI:
         laczna_liczba_kilogramow += produkt
         produkt = 0
-        continue
-    elif paczka == LIMIT_JEDNEJ_PACZKI :
-        laczna_liczba_kilogramow += produkt
+        print('Dodano produkt do paczki')
+    elif paczka == LIMIT_JEDNEJ_PACZKI:
         ilosc_wyslanych_paczek += 1
+        laczna_liczba_kilogramow += produkt
         paczka = 0
-        produkt = 0
-        continue
+        print('Paczka jest pełna, pobierz nową paczkę')
     elif paczka > LIMIT_JEDNEJ_PACZKI:
         paczka -= produkt
-        nadmiar = LIMIT_JEDNEJ_PACZKI - paczka
-        suma_pustych_kilogramow += nadmiar
+        print(paczka)
+        print(produkt)
+        suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI - paczka
+        print(suma_pustych_kilogramow)
         laczna_liczba_kilogramow += paczka
-        paczka += produkt
-        nadmiar = 0
         paczka = 0
-        produkt = 0
         ilosc_wyslanych_paczek += 1
-        continue
+        print('Dodano produkt do paczki')
+        print('Paczka jest pełna, pobierz nową paczkę')
+
 if ilosc_wyslanych_paczek <= 1 :
-    suma_pustych_kilogramow = LIMIT_JEDNEJ_PACZKI - paczka
+    suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI - paczka
+    ilosc_wyslanych_paczek = 1
+elif laczna_liczba_kilogramow == 20:
+    ilosc_wyslanych_paczek = 1
+else:
+    laczna_liczba_kilogramow += paczka
+    suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI -paczka
 
 # wyswietlenie wynikow
 
