f2b196c7d16a4447fcc0e596f84b7962d2ea26d4
alopacin <adlopacinski@gmail.com>
2023-05-28 22:44:20 +0200

    program dziala. Cztery wyniki wyswietlane w podsumowaniu wyliczane sa poprawnie

--- main.py
+++ main.py
@@ -1,64 +1,64 @@
 #nadanie zmiennych
-
 LIMIT_JEDNEJ_PACZKI = 20
-ilosc_wyslanych_paczek = 1
+ilosc_wyslanych_paczek = 0
 laczna_liczba_kilogramow = 0
 suma_pustych_kilogramow = 0
 paczka = 0
-najwieksza_ilosc_kilogramow =
+najwiecej_pustych_kilogramow = 0
+paczka_najwiecej_pustych_kilogramow = 0
+nadmiar = 0
 
 # wlasciwa czesc programu
 liczba_produktow = int(input('Ile produktów chcesz wysłać?: '))
 for _ in range(liczba_produktow):
-        produkt = 0
-        produkt = int(input('Podaj wagę produktu: '))
-        paczka += produkt
-
-        if produkt < 1 or produkt > 10:
-            break
-        elif paczka < LIMIT_JEDNEJ_PACZKI:
+    produkt = 0
+    produkt = int(input('Paczka: Jestem wytrzymała, pakuj na maksa\nPodaj wagę produktu: '))
+    paczka += produkt
+    if produkt < 1 or produkt > 10:
+        print('Produkt o nieodpowiedniej wadze!')
+        break
+    elif paczka < LIMIT_JEDNEJ_PACZKI:
+        laczna_liczba_kilogramow += produkt
+        continue
+    elif paczka >= LIMIT_JEDNEJ_PACZKI:
+        if paczka == LIMIT_JEDNEJ_PACZKI:
             laczna_liczba_kilogramow += produkt
-            print(f'Dodano produkt o wadze {produkt} kg do paczki')
-        elif paczka == LIMIT_JEDNEJ_PACZKI:
-            laczna_liczba_kilogramow += produkt
-            paczka = 0
             ilosc_wyslanych_paczek += 1
-            print('Paczka jest pełna, pobierz nową paczkę')
-        elif paczka > LIMIT_JEDNEJ_PACZKI:
+            paczka = 0
+        if paczka > LIMIT_JEDNEJ_PACZKI:
             paczka -= produkt
-            print(paczka)
-            print(produkt)
+            nadmiar = LIMIT_JEDNEJ_PACZKI - paczka
             suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI - paczka
-            print(suma_pustych_kilogramow)
             laczna_liczba_kilogramow += produkt
             ilosc_wyslanych_paczek += 1
             paczka = produkt
-            print(paczka)
-            print(produkt)
-            print(f'Dodano produkt o wadze {produkt} kg do paczki')
-            print('Paczka jest pełna, pobierz nową paczkę')
+            if nadmiar > najwiecej_pustych_kilogramow:
+                najwiecej_pustych_kilogramow = nadmiar
+                paczka_najwiecej_pustych_kilogramow = ilosc_wyslanych_paczek
+                nadmiar = 0
 
+if laczna_liczba_kilogramow <= 20 :
+        suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI - paczka
+        ilosc_wyslanych_paczek =  1
+        paczka_najwiecej_pustych_kilogramow = 1
+if laczna_liczba_kilogramow % 20  == 0 :
+        suma_pustych_kilogramow = 0
+if laczna_liczba_kilogramow > 20 and  laczna_liczba_kilogramow % 20  != 0 :
+        suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI - paczka
+        ilosc_wyslanych_paczek += 1
 
-if laczna_liczba_kilogramow == LIMIT_JEDNEJ_PACZKI:
-    ilosc_wyslanych_paczek = 1
-    suma_pustych_kilogramow = 0
-else:
-    suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI - paczka
-
-#elif produkt != 0:
-    #suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI -paczka
-    #laczna_liczba_kilogramow += produkt
+nadmiar = LIMIT_JEDNEJ_PACZKI - paczka
+if nadmiar > najwiecej_pustych_kilogramow:
+    paczka_najwiecej_pustych_kilogramow = ilosc_wyslanych_paczek
 
 # wyswietlenie wynikow
-
-if ilosc_wyslanych_paczek == 1:
-    print(f'Wysłano : {ilosc_wyslanych_paczek} paczkę')
-elif ilosc_wyslanych_paczek > 1 or ilosc_wyslanych_paczek < 4 :
-    print(f'Wysłano : {ilosc_wyslanych_paczek} paczki')
-else:
-    print(print(f'Wysłano : {ilosc_wyslanych_paczek} paczek'))
+print(f'Wysłano paczek : {ilosc_wyslanych_paczek} ')
 print(f'Wysłano : {laczna_liczba_kilogramow} kg')
 print(f'Suma pustych kilogramów : {suma_pustych_kilogramow}')
+if suma_pustych_kilogramow != 0:
+    print(f'Najwięcej pustych kilogramów ma paczka nr {paczka_najwiecej_pustych_kilogramow}[{nadmiar}kg]')
+else:
+    print('Wszystkie paczki zapakowane pod korek!')
 
 
 
