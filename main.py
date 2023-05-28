7198f482786864aa7b2ffb82c26d75e26d313c18
alopacin <adlopacinski@gmail.com>
2023-05-28 14:11:09 +0200

    W koncu dziala!Okazalo sie, ze wystarczylo usunac jeden znak i program zaczal dzialac jak powinien. Teraz dodac jedna funkcjonalnosc i gotowe

--- main.py
+++ main.py
@@ -4,49 +4,50 @@ LIMIT_JEDNEJ_PACZKI = 20
 ilosc_wyslanych_paczek = 1
 laczna_liczba_kilogramow = 0
 suma_pustych_kilogramow = 0
-nadmiar = 0
 paczka = 0
+najwieksza_ilosc_kilogramow =
 
 # wlasciwa czesc programu
 liczba_produktow = int(input('Ile produktów chcesz wysłać?: '))
 for _ in range(liczba_produktow):
-    produkt = int(input('Podaj wagę produktu: '))
-    paczka += produkt
-    print(paczka)
-    print(produkt)
-    print(laczna_liczba_kilogramow)
-    print(suma_pustych_kilogramow)
-    if produkt < 1 or produkt > 10:
-        break
-    elif paczka < LIMIT_JEDNEJ_PACZKI:
-        laczna_liczba_kilogramow += produkt
         produkt = 0
-        print('Dodano produkt do paczki')
-    elif paczka == LIMIT_JEDNEJ_PACZKI:
-        ilosc_wyslanych_paczek += 1
-        laczna_liczba_kilogramow += produkt
-        paczka = 0
-        print('Paczka jest pełna, pobierz nową paczkę')
-    elif paczka > LIMIT_JEDNEJ_PACZKI:
-        paczka -= produkt
-        print(paczka)
-        print(produkt)
-        suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI - paczka
-        print(suma_pustych_kilogramow)
-        laczna_liczba_kilogramow += paczka
-        paczka = 0
-        ilosc_wyslanych_paczek += 1
-        print('Dodano produkt do paczki')
-        print('Paczka jest pełna, pobierz nową paczkę')
+        produkt = int(input('Podaj wagę produktu: '))
+        paczka += produkt
 
-if ilosc_wyslanych_paczek <= 1 :
-    suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI - paczka
+        if produkt < 1 or produkt > 10:
+            break
+        elif paczka < LIMIT_JEDNEJ_PACZKI:
+            laczna_liczba_kilogramow += produkt
+            print(f'Dodano produkt o wadze {produkt} kg do paczki')
+        elif paczka == LIMIT_JEDNEJ_PACZKI:
+            laczna_liczba_kilogramow += produkt
+            paczka = 0
+            ilosc_wyslanych_paczek += 1
+            print('Paczka jest pełna, pobierz nową paczkę')
+        elif paczka > LIMIT_JEDNEJ_PACZKI:
+            paczka -= produkt
+            print(paczka)
+            print(produkt)
+            suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI - paczka
+            print(suma_pustych_kilogramow)
+            laczna_liczba_kilogramow += produkt
+            ilosc_wyslanych_paczek += 1
+            paczka = produkt
+            print(paczka)
+            print(produkt)
+            print(f'Dodano produkt o wadze {produkt} kg do paczki')
+            print('Paczka jest pełna, pobierz nową paczkę')
+
+
+if laczna_liczba_kilogramow == LIMIT_JEDNEJ_PACZKI:
     ilosc_wyslanych_paczek = 1
-elif laczna_liczba_kilogramow == 20:
-    ilosc_wyslanych_paczek = 1
+    suma_pustych_kilogramow = 0
 else:
-    laczna_liczba_kilogramow += paczka
-    suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI -paczka
+    suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI - paczka
+
+#elif produkt != 0:
+    #suma_pustych_kilogramow += LIMIT_JEDNEJ_PACZKI -paczka
+    #laczna_liczba_kilogramow += produkt
 
 # wyswietlenie wynikow
 
